93027242007e173f6f125b0bdbfda383eb52a558
emrekizil <emrekizildev@gmail.com>
2025-03-18 17:58:06 +0300

    social login response, onesignal send email

--- volvoxhub/src/main/java/com/volvoxmobile/volvoxhub/data/remote/model/hub/response/RegisterBaseResponse.kt
+++ volvoxhub/src/main/java/com/volvoxmobile/volvoxhub/data/remote/model/hub/response/RegisterBaseResponse.kt
@@ -7,8 +7,10 @@ data class RegisterBaseResponse(
     @SerializedName("status") val status: String,
     @SerializedName("message") val message: String,
     @SerializedName("vid") val vid: String,
+    @SerializedName("social") val social: RegisterSocialResponse,
     @SerializedName("device") val device: RegisterDeviceResponse,
     @SerializedName("config") val config: RegisterConfigResponse,
     @SerializedName("third_party") val thirdParty: RegisterThirdPartyResponse,
     @SerializedName("remote_config") val remoteConfig: JsonObject?,
+    @SerializedName("support") val support: RegisterSupportResponse
 )
